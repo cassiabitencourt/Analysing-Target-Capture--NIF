37137d44694b2a0545a25198ef727664605054ec
Cássia Bitencourt <45171352+cassiabitencourt@users.noreply.github.com>
2023-12-14 11:45:18 +0000

    1check_quality_sequencing.bash

--- 1check_quality_sequencing.bash
+++ 1check_quality_sequencing.bash
@@ -1,23 +1,32 @@
 #!/bin/bash
-#
 
-#SBATCH --chdir=/mnt/shared/projects/rbgk/projects/cbitencourt/Apocys/PAFTOL_1KP_NIF-CB
+# Set the working directory
+#SBATCH --chdir=/path/to/my/working/directory/on/the/HPC/cluster/where/all/the/raw/data/is/stored
+
+# Job parameters
 #SBATCH --job-name=fastqc
 #SBATCH --partition=medium
-#SBATCH --cpus-per-task=10
-#SBATCH --mem=24G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=16G
 #SBATCH --export=ALL
-#SBATCH --mail-user=c.bitencourt@kew.org
+#SBATCH --mail-user=email@kew.org
 #SBATCH --mail-type=END,FAIL
 
-for f in *.fastq.gz; 
-do (fastqc $f); 
+# Load required modules (modify as needed)
+module load fastqc
+module load multiqc
+
+# Run FastQC on all .fastq.gz files in the current directory
+for f in *.fastq.gz; do
+    fastqc "$f"
 done
 
-mkdir /mnt/shared/projects/rbgk/projects/cbitencourt/Apocys/PAFTOL_1KP_NIF-CB/fastqc_pre_trim
-mv *.zip /mnt/shared/projects/rbgk/projects/cbitencourt/Apocys/PAFTOL_1KP_NIF-CB/fastqc_pre_trim
-mv *.html /mnt/shared/projects/rbgk/projects/cbitencourt/Apocys/PAFTOL_1KP_NIF-CB/fastqc_pre_trim
+# Create a directory for storing FastQC results
+mkdir -p /path/to/my/working/directory/on/the/HPC/cluster/where/all/the/raw/data/is/stored/fastqc_pre_trim
 
-#Use MultiQC to collate all QC information into one place, and to generate useful output files for all samples
-#https://protect-eu.mimecast.com/s/KNJjCJNR8HBXL6VuGJ4GB/
-multiqc ./ ### done on every folder with fastqc reports in it. I.e., done for each of the 15 sequencing runs.
+# Move FastQC output files to the new directory
+mv *.zip *.html /path/to/my/working/directory/on/the/HPC/cluster/where/all/the/raw/data/is/stored/fastqc_pre_trim
+
+# Use MultiQC to collate all QC information into one place
+# Run MultiQC on the directory containing FastQC results
+multiqc /path/to/my/working/directory/on/the/HPC/cluster/where/all/the/raw/data/is/stored/fastqc_pre_trim
