397496590d9ea65e548373bd8fce24e36e681d3f
Cássia Bitencourt <45171352+cassiabitencourt@users.noreply.github.com>
2023-12-25 21:22:52 +0000

    Update 4.0_Do_assembly_using_HybPiper.bash

--- 4.0_Do_assembly_using_HybPiper.bash
+++ 4.0_Do_assembly_using_HybPiper.bash
@@ -1,53 +1,38 @@
 #!/bin/bash
-#
-#SBATCH--chdir=/mnt/shared/projects/rbgk/projects/cbitencourt/Apocys/PAFTOL_1KP_NIF-CB/assembly
+
+#SBATCH --chdir=/path/to/my/working/directory/on/the/HPC/cluster/where/all/the/raw/data/is/stored/assembly
 #SBATCH --job-name=HybPiper
 #SBATCH --partition=long
-#SBATCH --array=1-342%15   
+#SBATCH --array=1-353%10   
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=24G
-#SBATCH --mail-user=c.bitencourt@kew.org
+#SBATCH --mail-user=email@kew.org
 #SBATCH --mail-type=END,FAIL
 
-
+# Print the SLURM array task ID
 echo $SLURM_ARRAY_TASK_ID
 
-name=$(awk -v lineid=$SLURM_ARRAY_TASK_ID 'NR==lineid{print;exit}' /mnt/shared/projects/rbgk/projects/cbitencourt/Apocys/PAFTOL_1KP_NIF-CB/assembly/Apocynaceaelist.txt)
+# Retrieve sample name from FamilyList.txt using SLURM array task ID
+name=$(awk -v lineid=$SLURM_ARRAY_TASK_ID 'NR==lineid{print;exit}' /path/to/my/working/directory/on/the/HPC/cluster/where/all/the/raw/data/is/stored/assembly/FamilyList.txt)
 
+# Print the sample name
 echo $name
 
-
-## We need to do hybpiper on cropdiversity's local scratch because HybPiper produces A LOT of temporary files and folders.
-## The local scratch directory is a directory that is temporary and only produced while the analysis is running.
-## We then have to copy all of the results into our own folder before the script ends, otherwise everything from that run is deleted when the local scratch is deleted automatically.
-## see https://help.cropdiversity.ac.uk/data-storage.html#local-scratch 
-
+# Change to the temporary directory
 cd $TMPDIR
 
-
-#----- Assembling targetted angio353 baits loci - Run Hypiper ----- 
-#### YOU NEED TO CHECK IF YOUR FILES AFTER TRIM ARE ZIPPED, IF YES ... ####
-
-#### FIRST, UNZIP THE READ FILES ####
-#parallel -j12 "gunzip {}" ::: *.fastq.gz
-
-
-#Run hybpiper
-
+# Assemble targeted angio353 baits loci using HybPiper
 hybpiper assemble \
-  -t_dna /mnt/shared/projects/rbgk/projects/cbitencourt/Apocys/working/apocynaceae_targetfile.fasta \
-  --readfiles /mnt/shared/projects/rbgk/projects/cbitencourt/Apocys/PAFTOL_1KP_NIF-CB/trimmed_file/"$name"_R1_Tpaired.fastq.gz /mnt/shared/projects/rbgk/projects/cbitencourt/Apocys/PAFTOL_1KP_NIF-CB/trimmed_file/"$name"_R2_Tpaired.fastq.gz \
+  -t_dna /path/to/my/working/directory/on/the/HPC/cluster/where/all/the/target/file/is/stored/targetfile.fasta \
+  --readfiles /path/to/directory/where/all/the/trimmed/data/is/stored/trimmed_file/"$name"_R1_Tpaired.fastq.gz /mnt/shared/projects/rbgk/projects/cbitencourt/Apocys/PAFTOL_1KP_NIF-CB/trimmed_file/"$name"_R2_Tpaired.fastq.gz \
   --prefix $name \
   --bwa \
   --cov_cutoff 8 \
   --cpu ${SLURM_CPUS_PER_TASK}
 
-# At this stage you have a folder per sample, and in each sample you have a folder per gene, containing the corresponding concatenated exons sequences for this gene for this sample.
-
-
-## Copy all the results back into your own folder
-#Copy folders
-parallel 'cp -r {} /mnt/shared/projects/rbgk/projects/cbitencourt/Apocys/PAFTOL_1KP_NIF-CB/assembly/' ::: *
-#Copy files
-parallel 'cp {} /mnt/shared/projects/rbgk/projects/cbitencourt/Apocys/PAFTOL_1KP_NIF-CB/assembly/' ::: *
+# Copy results back to the original folder
+# Copy folders
+parallel 'cp -r {} /path/to/my/working/directory/on/the/HPC/cluster/where/all/the/raw/data/is/stored/assembly/' ::: *
+# Copy files
+parallel 'cp {} /path/to/my/working/directory/on/the/HPC/cluster/where/all/the/raw/data/is/stored/assembly/' ::: *
